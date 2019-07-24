ab200a4795a3a7917e8cba35546d5ec131c2702e
Andrew <andykuo1supergreen@gmail.com>
2019-07-24 16:30:08 -0700

    Removed unused id and files

--- src/test03/Assignment.js
+++ src/test03/Assignment.js
@@ -1,12 +1,10 @@
 /**
- * @param {String} assignmentID The id for the assignment.
  * @param {Date} dueDate The date the assignment is due.
  * @param {Object} attributes Any additional attributes for the assignment.
  */
-function createAssignment(assignmentID, dueDate, attributes={})
+function createAssignment(dueDate, attributes={})
 {
     return {
-        id: assignmentID,
         dueDate,
         attributes
     };
