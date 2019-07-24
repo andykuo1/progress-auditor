fb0899c8014d9cefefa6eb8fbbfe4332f23617ac
Andrew <andykuo1supergreen@gmail.com>
2019-07-24 16:37:51 -0700

    Refactored everything

--- src/database/database.js
+++ src/database/database.js
@@ -1,21 +1,30 @@
+/**
+ * Creates a database to hold all your data :)
+ */
 function createDatabase()
 {
     return {
-        user: new Map(),
-        submission: new Map(),
-        review: new Map(),
-        vacation: new Map(),
+        _errors: [],
+        throwError(src, error, ...messages)
+        {
+            console.error(src, error, ...messages);
+            this._errors.push({
+                source: src,
+                error,
+                message: messages.join(' ')
+            });
+        },
+        clearErrors()
+        {
+            this._errors.length = 0;
+        },
+        getErrors()
+        {
+            return this._errors;
+        }
     };
 }
 
-function getUserByID(database, id)
-{
-    database.user.get(id);
-}
-
-db.user(userID).getPosts();
-db.user(userID).getEmail();
-db.user(userID).getName();
-db.user(userID).getVacations();
-db.user(userID).getPosts(new Date(), new Date());
-db.user(userID).getUsedSlipDays();
+module.exports = {
+    createDatabase
+};
