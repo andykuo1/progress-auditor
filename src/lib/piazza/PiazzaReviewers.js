f4b5aec871f00a0b55a1978593873223ef413fb8
Andrew <andykuo1supergreen@gmail.com>
2019-08-23 20:34:34 -0700

    Added add_submission reviewer

--- src/lib/piazza/PiazzaReviewers.js
+++ src/lib/piazza/PiazzaReviewers.js
@@ -2,16 +2,18 @@ import * as NullReviewer from '../../lib/piazza/reviewer/NullReviewer.js';
 import * as SubmissionChangeAssignmentReviewer from '../../lib/piazza/reviewer/SubmissionChangeAssignmentReviewer.js';
 import * as SubmissionIgnoreOwnerReviewer from '../../lib/piazza/reviewer/SubmissionIgnoreOwnerReviewer.js';
 import * as SubmissionIgnoreReviewer from '../../lib/piazza/reviewer/SubmissionIgnoreReviewer.js';
+import * as SubmissionAddReviewer from '../../lib/piazza/reviewer/SubmissionAddReviewer.js';
 import * as UserAddOwnerKeyReviewer from '../../lib/piazza/reviewer/UserAddOwnerKeyReviewer.js';
 
 const REGISTRY = new Map();
 
-// Order matters!
+// Order does NOT matter!
 REGISTRY.set(NullReviewer.REVIEW_ID, NullReviewer);
+REGISTRY.set(UserAddOwnerKeyReviewer.REVIEW_ID, UserAddOwnerKeyReviewer);
 REGISTRY.set(SubmissionChangeAssignmentReviewer.REVIEW_ID, SubmissionChangeAssignmentReviewer);
 REGISTRY.set(SubmissionIgnoreOwnerReviewer.REVIEW_ID, SubmissionIgnoreOwnerReviewer);
 REGISTRY.set(SubmissionIgnoreReviewer.REVIEW_ID, SubmissionIgnoreReviewer);
-REGISTRY.set(UserAddOwnerKeyReviewer.REVIEW_ID, UserAddOwnerKeyReviewer);
+REGISTRY.set(SubmissionAddReviewer.REVIEW_ID, SubmissionAddReviewer);
 
 export {
     REGISTRY
---
+++ src/lib/piazza/reviewer/SubmissionAddReveiwer.js
@@ -0,0 +1,20 @@
+import * as AssignmentDatabase from '../../../database/AssignmentDatabase.js';
+import * as SubmissionDatabase from '../../../database/SubmissionDatabase.js';
+import * as ParseUtil from '../../../util/ParseUtil.js';
+import { stringHash } from '../../../util/MathHelper.js';
+
+export const REVIEW_ID = 'add_submission';
+
+export async function review(db, reviewID, reviewType, reviewParams)
+{
+    const ownerKey = reviewParams[0];
+    const assignmentID = reviewParams[1];
+    const submissionID = ownerKey + '#proxy_' + stringHash(`${reviewID}:${ownerKey}.${assignmentID}`);
+    const submissionDate = reviewParams.length >= 2
+        ? ParseUtil.parseAmericanDate(reviewParams[2])
+        : new Date(AssignmentDatabase.getAssignmentByID(db, userID, assignmentID).dueDate.getTime() - 1);
+    const submissionAttributes = reviewParams.length >= 3
+        ? JSON.parse(reviewParams[3])
+        : {};
+    SubmissionDatabase.addSubmission(db, submissionID, ownerKey, assignmentID, submissionDate, submissionAttributes);
+}
