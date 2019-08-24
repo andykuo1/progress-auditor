47ec70d6684dc14816046ee4662681f1e8f5da39
Andrew <andykuo1supergreen@gmail.com>
2019-08-23 18:06:34 -0700

    Removed unused reviewers and resolvers

--- dist/lib/cse197-piazza/reviewer/unknown-reviewer.js
+++
@@ -1,8 +0,0 @@
-async function review(db, reviewID, reviewType, reviewParams)
-{
-    db.throwError(`[UNKNOWN_REVIEW] Unknown review type '${reviewType}'.`);
-}
-
-module.exports = {
-    review
-};
