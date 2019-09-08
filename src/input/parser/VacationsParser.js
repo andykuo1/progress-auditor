c13edb504a5d2158cf69011ccf3518a6749523b5
Andrew <andykuo1supergreen@gmail.com>
2019-09-08 16:34:10 -0700

    Implemented review and vacation parsers

---
+++ src/input/parser/VacationsParser.js
@@ -0,0 +1,44 @@
+import * as VacationDatabase from '../../database/VacationDatabase.js';
+import * as FileUtil from '../../util/FileUtil.js';
+import * as ParseUtil from '../../util/ParseUtil.js';
+import * as FieldParser from '../../util/FieldParser.js';
+
+/**
+ * Create VacationDatabase based on input file.
+ * @param {Database} db The database to write to.
+ * @param {String} filepath The path to the file to parse.
+ * @param {Object} opts Any additional options.
+ */
+export async function parse(db, filepath, opts={})
+{
+    VacationDatabase.setupDatabase(db);
+
+    let first = true;
+    await FileUtil.readCSVFileByRow(filepath, (row) => {
+        // Skip header...
+        if (first) { first = false; return; }
+
+        // 0. Vacation ID
+        // 1. User ID
+        // 2. Start Date
+        // 3. End Date
+        // 4. Padding
+        // ...
+        try
+        {
+            const vacationID = row[0];
+            const ownerKey = FieldParser.parseEmail(row[1]);
+            const startDate = ParseUtil.parseAmericanDate(row[2]);
+            const endDate = ParseUtil.parseAmericanDate(row[3]);
+            const padding = row[4];
+
+            const vacation = VacationDatabase.addVacation(db, vacationID, ownerKey, startDate, endDate, padding);
+        }
+        catch(e)
+        {
+            db.throwError('PARSE', 'Unable to parse vacations - ' + e);
+        }
+    });
+
+    return db;
+}
