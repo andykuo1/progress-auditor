a5bddb2e598b87e5b72690b20cfab22255333445
Andrew <andykuo1supergreen@gmail.com>
2019-09-08 16:53:28 -0700

    Moved stage comments to ClientApplication

--- src/app/client/ClientApplication.js
+++ src/app/client/ClientApplication.js
@@ -19,6 +19,13 @@ export async function onStart(args)
 
 export async function onSetup(db, config)
 {
+    /**
+     * Loading - Where all data should be loaded from file. This
+     * should be raw data as defined by the user. No modifications
+     * should take place; they will be considered for alterations
+     * in the processing stage.
+     */
+
     // Prepare registries from scheme...
     // NOTE: Any new schemes should be added to prepareScheme().
     await prepareScheme(db, config);
@@ -29,6 +36,18 @@ export async function onSetup(db, config)
 
 export async function onPreProcess(db, config)
 {
+    /**
+     * Processing - Where all data is evaluated and processed into
+     * valid and useful information. This is also where most of the
+     * errors not related to IO will be thrown. This stage consists
+     * of two steps: the review and the resolution. The resolution
+     * step will attempt to automatically format and validate the
+     * data. If it is unable to then the data is invalid and is
+     * flagged for review by the user. Therefore, the review step,
+     * which processes all user-created reviews, is computed before
+     * the resolution. This is a frequent debug loop.
+     */
+    
     await runProcessors(db, config, false);
     
     // Review resolution loop
@@ -86,7 +105,11 @@ export async function onPostProcess(db, config)
 
 export async function onOutput(db, config)
 {
-
+    /**
+     * Outputting - Where all data is outputted into relevant
+     * files. If any errors had occured, it will exit-early and
+     * output any gathered debug information.
+     */
 }
 
 export async function onError(db, config, error)
--- src/main.js
+++ src/main.js
@@ -18,47 +18,20 @@ export async function main(args)
     let db;
     try
     {
-        /**
-         * Loading - Where all data should be loaded from file. This
-         * should be raw data as defined by the user. No modifications
-         * should take place; they will be considered for alterations
-         * in the processing stage.
-         */
-
         // Starting setup...
         config = await MainApplication.resolveConfig(DIRECTORY);
         db = await MainApplication.resolveDatabase(config);
 
         await ClientApplication.onSetup(db, config);
-
-        /**
-         * Processing - Where all data is evaluated and processed into
-         * valid and useful information. This is also where most of the
-         * errors not related to IO will be thrown. This stage consists
-         * of two steps: the review and the resolution. The resolution
-         * step will attempt to automatically format and validate the
-         * data. If it is unable to then the data is invalid and is
-         * flagged for review by the user. Therefore, the review step,
-         * which processes all user-created reviews, is computed before
-         * the resolution. This is a frequent debug loop.
-         */
         await ClientApplication.onPreProcess(db, config);
 
         // All setup is GUARANTEED to be done now. Reviews are now to be processed...
         // Any errors that dare to surface will be vanquished here. Or ignored...
         await MainApplication.validateDatabase(db, config);
-
         await ClientApplication.onPostProcess(db, config);
     
-        /**
-         * Outputting - Where all data is outputted into relevant
-         * files. If any errors had occured, it will exit-early and
-         * output any gathered debug information.
-         */
-
         // All validation is GUARANTEED to be done now. Outputs can now be generated...
         await MainApplication.generateOutput(db, config);
-
         await ClientApplication.onOutput(db, config);
     }
     catch(e)
