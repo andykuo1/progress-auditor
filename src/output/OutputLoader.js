d5da41a212e2cbab36d429a6ba8b1817ed75f08b
Andrew <andykuo1supergreen@gmail.com>
2019-09-24 14:07:09 -0700

    Added some docs for OutputLoader

--- src/output/OutputLoader.js
+++ src/output/OutputLoader.js
@@ -1,8 +1,11 @@
 export function getOutputTypes()
 {
     return [
+        // The general expected output with all users and their slip days.
         'instructor',
+        // The specific summary for each user, including options for pdf versions.
         'student',
+        // The debug logs.
         'debug',
     ];
 }
