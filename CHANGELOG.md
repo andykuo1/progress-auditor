f868ea1e7c1b8f7b25bb119bdc17d49a1d35ebe9
Andrew Kuo <andykuo1supergreen@gmail.com>
2020-07-09 09:28:25 -0700

    Bumped version to v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,8 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [1.0.2] - 2020-06-09
 ### Fixed
-- Fixed undefined access of "type" for review selection due to type mismatch
+- Fixed undefined access of "type" for review selection due to type mismatch from enquirer
 
 ## [1.0.1] - 2020-06-07
 ### Added
--- dist/src/bundle.js
+++ dist/src/bundle.js
@@ -12630,6 +12630,103 @@ var CohortParser = /*#__PURE__*/Object.freeze({
     parse: parse$1
 });
 
+// TODO: not used yet.
+const PARSER_OPTIONS$1 = {
+    maxEndDates: {
+        type: "Array<{ pattern, endDate }>",
+        description: "An array of max end date entries that, if the specified pattern matched, will limit the user's end date to the specified date."
+    }
+};
+
+/**
+ * Create UserDatabase based on input file.
+ * @param {Database} db The database to write to.
+ * @param {Config} config The program config.
+ * @param {String} filepath The path to the file to parse.
+ * @param {Object} opts Any additional options.
+ */
+async function parse$2(db, config, filepath, opts={ maxEndDates: [] })
+{
+    setupDatabase$1(db);
+
+    const maxEndDateEntries = processMaxEndDateEntries$1(opts.maxEndDates);
+
+    let first = true;
+    await readCSVFileByRow(filepath, (row) => {
+        // Skip header...
+        if (first) { first = false; return; }
+        //Company ,Company location,Which quarter will you enroll in CSE 197?,"Does your internship start *before* June 17, 2019",Start date of internship,Anticipated end date of internship,Expected UCSD Graduation,How many CSE internships have you already completed?,How did you hear about your internship?,Do you have an offer of employment at a CSE internship,Is your major in CSE?,"Does your internship satisfy all of the following criteria: (1) projects are related to your CSE major, (2) full-time (at least 30 hours of work each week and at least 8 weeks long), (3) paid, (4) on-site (at the company's place of business; tele-commuting internships do not qualify), (5) with a designated company mentor / supervisor.",Have you enrolled in CSE 197 before?,Will you be part of a cohort of interns at your company participating in an established internship program?,Will this be your first time employed by this company?,Are you planning to be in San Diego in the Fall (following your internship)?,Additional explanation or extenuating circumstances?,Are you required to complete and sign a CPT form?,EASy Dept approval,6 month coop,Max slip days allowed,Slip days used as of 7.8,Latest reflection as of 7.8.19,7.12.19 Notes,Slip days used as of 7.15,Latest reflection as of 7.15.19,7.15.19 Notes
+        // 0. Timestamp
+        // 1. Email Address (not sure what this is for)
+        // 2. Last name
+        // 3. First name
+        // 4. Display name
+        // 5. PID
+        // 6. Email
+        // 7. Other Email
+        // ...
+        // 9. Which quarter will you enroll in CSE 197?
+        // ...
+        // 12. Start date of internship
+        // 13. Anticipated end date of internship
+        // ...
+        // 24. EASy approval date
+        // 25. Admin notes
+        // 28. Max slip days allowed (giving 4 instead of 3 per week to account for time zone)
+        // ...
+        try
+        {
+            const userID = row[5].trim().toUpperCase(); // FieldParser.parseEmail(row[6]);
+            const ownerKey = parseEmail(row[6], row[1], row[7]);
+            const userName = parseName(`${row[3]} ${row[2]}`);
+            const pid = row[5].trim().toUpperCase();
+            const startDate = parseAmericanDate(row[12]);
+            const endDate = parseAmericanDate(row[13]);
+
+            // Max end date option...
+            const enrollmentQuarter = row[9];
+            for(const maxEndDateEntry of maxEndDateEntries)
+            {
+                if (maxEndDateEntry.pattern.test(enrollmentQuarter))
+                {
+                    if (compareDates(endDate, maxEndDateEntry.endDate) > 0)
+                    {
+                        endDate.setTime(maxEndDateEntry.endDate.getTime());
+                    }
+                }
+            }
+            const user = addUser(db, userID, ownerKey, userName, startDate, endDate, opts, { pid });
+        }
+        catch(e)
+        {
+            db.throwError('PARSE', 'Unable to parse users - ' + e);
+        }
+    });
+
+    return db;
+}
+
+function processMaxEndDateEntries$1(maxEndDates)
+{
+    if (!Array.isArray(maxEndDates)) return [];
+    if (maxEndDates.length <= 0) return [];
+
+    const result = [];
+    for(const maxEndDate of maxEndDates)
+    {
+        result.push({
+            pattern: new RegExp(maxEndDate.pattern),
+            endDate: parse(maxEndDate.endDate),
+        });
+    }
+    return result;
+}
+
+var CohortParser2020 = /*#__PURE__*/Object.freeze({
+    PARSER_OPTIONS: PARSER_OPTIONS$1,
+    parse: parse$2
+});
+
 function evaluatePostAssignment(headerContent, bodyContent)
 {
     // Intro Assignment
@@ -12761,7 +12858,7 @@ Result:
  * @param {String} filepath The path to the file to parse.
  * @param {Object} opts Any additional options.
  */
-async function parse$2(db, config, filepath, opts={})
+async function parse$3(db, config, filepath, opts={})
 {
     setupDatabase$2(db);
 
@@ -12831,7 +12928,7 @@ async function parse$2(db, config, filepath, opts={})
 }
 
 var ContributionsParser = /*#__PURE__*/Object.freeze({
-    parse: parse$2
+    parse: parse$3
 });
 
 /**
@@ -12840,7 +12937,7 @@ var ContributionsParser = /*#__PURE__*/Object.freeze({
  * @param {String} filepath The path to the file to parse.
  * @param {Object} opts Any additional options.
  */
-async function parse$3(db, config, filepath, opts={})
+async function parse$4(db, config, filepath, opts={})
 {
     setupDatabase$3(db);
 
@@ -12893,7 +12990,7 @@ async function parse$3(db, config, filepath, opts={})
 }
 
 var ReviewsParser = /*#__PURE__*/Object.freeze({
-    parse: parse$3
+    parse: parse$4
 });
 
 function getParserTypes()
@@ -12910,6 +13007,7 @@ function loadParserByType(parserType)
     switch(parserType)
     {
         case 'cohort': return CohortParser;
+        case 'cohort2020': return CohortParser2020;
         case 'contributions': return ContributionsParser;
         case 'reviews': return ReviewsParser;
         default:
@@ -14645,7 +14743,7 @@ async function generateOutput(db, config)
     }
 }
 
-const version = "1.0.1";
+const version = "1.0.2";
 
 /**
  * This file handles the extra client interface of the program.
